ecb80882f3b838adbea7d40460ca2d6a4c295a0b
sketchnoobdeveloper <sketchnoobdeveloper@users.noreply.github.com>
2026-08-23 14:27:23 +0000

    Arcade: nanosvg resolves from 3rdparty root

--- emulator/android-MAME4droid/app/src/main/jni/Core.mk
+++ emulator/android-MAME4droid/app/src/main/jni/Core.mk
@@ -97,8 +97,7 @@ LOCAL_C_INCLUDES := \
     $(MROOT)/3rdparty/glm \
     $(MROOT)/3rdparty/compat \
     $(MROOT)/3rdparty/asio/include \
-    $(MROOT)/3rdparty/nanosvg/src \
-    $(MROOT)/3rdparty/nanosvgrast/src \
+    $(MROOT)/3rdparty \
     $(MROOT)/bgfx/include \
     $(LOCAL_PATH)
 
